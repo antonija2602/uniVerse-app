cb6e6e01b823d6d87dd4cf787057967e7f841e7c
Antonija Zrno <antonija.zrno@gmx.de>
2024-04-06 14:00:05 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main"
+import SideBar from "./components/SideBar"
+import Footer from "./components/Footer"
+
+export default function App() {
+    return (
+        <>
+            <Main />
+            <SideBar />
+            <Footer />
+        </>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <>
+            <img
+                src="uni-2.png"
+                alt="universe-picture"
+            />
+        </>
+    )
+}
